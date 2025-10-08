29aa6c83db5eef10a6cd25f12ff7745001aec9ce
Maxim Pushkar <archon@mail.ru>
2025-10-08 14:19:04 +0300

    lazy schema parse

--- lib/xod/misc.ex
+++ lib/xod/misc.ex
@@ -114,3 +114,33 @@ defmodule Xod.Time do
     def parse(_, value, path), do: {:error, Xod.XodError.invalid_type(:time, Xod.Common.get_type(value), path)}
   end
 end
+
+defmodule Xod.Lazy do
+  @type t() :: %__MODULE__{callee: {module(), atom(), [any()]} | {fun(), [any()]}}
+
+  @enforce_keys [:callee]
+  defstruct [:callee]
+
+  @spec new(module(), atom(), [any()]) :: t()
+  def new(m, f, a) when is_atom(m) and is_atom(f) and is_list(a) do
+    %__MODULE__{callee: {m, f, a}}
+  end
+
+  @spec new(fun(), [any()]) :: t()
+  def new(f, a) when is_function(f) and is_list(a) do
+    %__MODULE__{callee: {f, a}}
+  end
+
+  defimpl Xod.Schema do
+    @impl true
+    def parse(%Xod.Lazy{callee: {m, f, a}}, value, path) do
+      schema = Kernel.apply(m, f, a)
+      Xod.Schema.parse(schema, value, path)
+    end
+
+    def parse(%Xod.Lazy{callee: {f, a}}, value, path) do
+      schema = Kernel.apply(f, a)
+      Xod.Schema.parse(schema, value, path)
+    end
+  end
+end

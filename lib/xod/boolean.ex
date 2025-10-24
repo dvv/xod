46fa672429611bacc769f06cc276b533922a2e2f
Vladimir Dronnikov <dronnikov@gmail.com>
2025-10-24 05:31:15 +0300

    revisit boolean

--- lib/xod/boolean.ex
+++ lib/xod/boolean.ex
@@ -8,20 +8,20 @@ defmodule Xod.Boolean do
 
   defimpl Xod.Schema do
 
+    @impl true
     def parse(%Xod.Boolean{coerce: true} = schema, value, path) when is_binary(value) do
-      cond do
-        value in ["true"] -> Xod.Schema.parse(%{schema | coerce: false}, true, path)
-        value in ["false"] -> Xod.Schema.parse(%{schema | coerce: false}, false, path)
-        true -> Xod.Schema.parse(%{schema | coerce: false}, value, path)
+      value = cond do
+        value in ["true", "1"] -> true
+        value in ["false", "0"] -> false
+        true -> value
       end
+      Xod.Schema.parse(%{schema | coerce: false}, value, path)
     end
 
-    @impl true
     def parse(%Xod.Boolean{coerce: false}, value, path) when not is_boolean(value) do
       {:error, Xod.XodError.invalid_type(:boolean, Xod.Common.get_type(value), path)}
     end
 
-    @impl true
     def parse(_, value, _) do
       if value, do: {:ok, true}, else: {:ok, false}
     end
